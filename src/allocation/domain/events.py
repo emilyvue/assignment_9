cec536943eac5fea187443fa2ae5f24e95d193d7
Emily Vue <emilyvue@Emilys-MacBook-Pro.local>
2023-05-01 17:39:55 +0900

    update

---
+++ src/allocation/domain/events.py
@@ -0,0 +1,28 @@
+# pylint: disable=too-few-public-methods
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+
+class Event:
+    pass
+
+
+@dataclass
+class Allocated(Event):
+    orderid: str
+    sku: str
+    qty: int
+    batchref: str
+
+
+@dataclass
+class Deallocated(Event):
+    orderid: str
+    sku: str
+    qty: int
+
+
+@dataclass
+class OutOfStock(Event):
+    sku: str
